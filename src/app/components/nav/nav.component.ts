f9f97327cd56dae5a2758180e3fae7015f1c9a43
Isabel Rebollo <95927789+isarebollo@users.noreply.github.com>
2023-05-30 12:21:38 +0200

    nav creado

--- src/app/components/nav/nav.component.ts
+++ src/app/components/nav/nav.component.ts
@@ -1,10 +1,79 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-nav',
   templateUrl: './nav.component.html',
   styleUrls: ['./nav.component.css']
 })
-export class NavComponent {
+export class NavComponent implements OnInit {
 
+
+  color: string = "";
+  searchText = '';
+  showSearch = true;
+  pokemonService: any;
+  constructor() { }
+  ngOnInit(): void {
+    this.color = this.pokemonService.activePokemon.subscribe(
+      (response: { color: string; } | null) => {
+        if (response === null) {
+          this.color = 'navbar';
+          this.showSearch = true;
+        } else {
+          this.color = response.color;
+          this.showSearch = false;
+        }
+        this.setTitleBarColor(this.color);
+      }
+    );
+  }
+  setTitleBarColor(color: string) {
+    let hexColor = '#FFFFFF';
+    switch (color) {
+      case 'black': {
+        hexColor = '#607d8b';
+        break;
+      }
+      case 'blue': {
+        hexColor = '#81d4fa';
+        break;
+      }
+      case 'brown': {
+        hexColor = '#bcaaa4';
+        break;
+      }
+      case 'gray': {
+        hexColor = '#a6a6a6';
+        break;
+      }
+      case 'green': {
+        hexColor = '#81c784';
+        break;
+      }
+      case 'pink': {
+        hexColor = '#f8bbd0';
+        break;
+      }
+      case 'purple': {
+        hexColor = '#ad8ee7';
+        break;
+      }
+      case 'red': {
+        hexColor = '#ff8a80';
+        break;
+      }
+      case 'white': {
+        hexColor = '#d5dbe1';
+        break;
+      }
+      case 'yellow': {
+        hexColor = '#ffd600';
+        break;
+      }
+      default: {
+        hexColor = '#FFFFFF';
+      }
+
+    }
+  }
 }
